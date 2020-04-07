72cb927e933262c9f424fbf6b1ca77161e1d91a8
mahoneynomadic <50168663+mahoneynomadic@users.noreply.github.com>
2020-04-07 15:37:57 +0200

    Update censor_dispenser.py

--- censor_dispenser.py
+++ censor_dispenser.py
@@ -1,44 +1,70 @@
-with open('email_one.txt') as email:
-    email_one = email.read()
-with open('email_two.txt') as email:
-    email_two = email.read()
-with open('email_three.txt') as email:
-    email_three = email.read()
-with open('email_four.txt') as email:
-    email_four = email.read()
-#Write a function that can censor a specific word or
-#phrase from a body of text, and then return the text.
-#Mr. Cloudy has asked you to use the function to
-#censor all instances of the phrase learning algorithms from the first email, email_one. Mr. Cloudy doesn’t care how you censor it, he just wants it done.
+email_one = open("/Users/mahmutozmen/Desktop/censor_dispenser/email_one.txt", "r").read()
+email_two = open("/Users/mahmutozmen/Desktop/censor_dispenser/email_two.txt", "r").read()
+email_three = open("/Users/mahmutozmen/Desktop/censor_dispenser/email_three.txt", "r").read()
+email_four = open("/Users/mahmutozmen/Desktop/censor_dispenser/email_four.txt", "r").read()
 
 
-def censor(word):
+def censor1(email,word):
 
-    return email_one.replace('learning algorithms','...')
-
-#print(censor('email_one.txt'))
-
+    return email.replace(word,'*'*len(word))
 
 
 proprietary_terms = ["she", "personality matrix", "sense of self", "self-preservation", "learning algorithm", "her", "herself"]
-#for i in proprietary_terms:
- #   if i=='she':
-  #      print(i)
-def censor1(word):
-    for i in email_two.split():
-        for m in proprietary_terms:
-            if i==m:
-                return email_two.replace(i,'---')
-#print(censor1(proprietary_terms))
+
+def censor_list_of_words(email,word_list):
+        objected_list = ['']
+        objected_list[0] = email
+        for i in word_list:
+            if i in objected_list[0]:
+                objected_list[0]=objected_list[0].replace(i,'REDACTED')
+        return objected_list[0]
+
+
+
 
 negative_words = ["concerned", "behind", "danger", "dangerous", "alarming", "alarmed", "out of control", "help", "unhappy", "bad", "upset", "awful", "broken", "damage", "damaging", "dismal", "distressed", "distressed", "concerning", "horrible", "horribly", "questionable"]
 
-def censor3(lakirti):
-    for p in email_three.split():
-        for k in lakirti:
-            if p==k:
-                return email_three.replace(p,'###')
+def censor_two_lists_of_words(phrases,email,after=0):
 
-#print(censor3('email_three.txt'))
+    objected_list = ['']
 
-#print(email_three)
+    objected_list[0] = email
+
+    for i in phrases:
+
+        if i in objected_list[0]:
+
+            object_variable = objected_list[0].find(i)
+
+            for j in phrases:
+                if j in objected_list[0][object_variable+1:]:
+                    objected_list[0] = objected_list[0][:object_variable + 1] + objected_list[0][object_variable + 1:].replace(i, 'REDACTED')
+
+    return censor_list_of_words(objected_list[0],proprietary_terms)
+
+
+def censoreAll(email):
+  objected_list = ['']
+  objected_list2 = ['']
+  objected_list3 = []
+  objected_list[0] = email
+  objected_list[0] = censor_list_of_words(objected_list[0], proprietary_terms)
+  objected_list[0] = censor_list_of_words(objected_list[0], negative_words)
+  objected_list2[0] = objected_list[0].replace('\n', ' lineBreak ')
+  objected_list3 = objected_list2[0].split(' ')
+  y = 0
+  while y < len(objected_list3) - 1:
+    if (objected_list3[y] == 'REDACTED') or (objected_list3[y] == 'REDACTEDe') or (objected_list3[y] == 'REDACTEDous') or (objected_list3[y] == 'REDACTEDly') or (objected_list3[y] == 'REDACTED\'s') or (objected_list3[y] == 'REDACTED.') or (objected_list3[y] == 'REDACTEDs') or (objected_list3[y] == 'REDACTED!') or (objected_list3[y] == 'REDACTEDe.'):
+      if y == 0 and (not objected_list3[1] == 'lineBreak'):
+        objected_list3[y + 1] = 'REDACTED'
+        continue
+      if not y == (objected_list3.index(objected_list3[-1])):
+        if not objected_list3[y + 1] == 'lineBreak':
+          objected_list3[y + 1] = 'REDACTED'
+        if not objected_list3[y - 1] == 'lineBreak':
+          objected_list3[y - 1] = 'REDACTED'
+      y += 2
+    y += 1
+  objected_list[0] = ' '.join(objected_list3)
+  objected_list[0] = objected_list[0].replace('lineBreak', '\n')
+  return objected_list[0]
